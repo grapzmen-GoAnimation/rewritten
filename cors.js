72903bbbd8238edc37ebb76e4fab9f49be05ef67
grapzmen-GoAnimation <arthurhenriqueesr@gmail.com>
2025-11-21 21:19:36 -0300

    Update cors.js

--- cors.js
+++ cors.js
@@ -1,5 +1,4 @@
 app.use(function(req, res, next) {
-     res.header("Access-Control-Allow-Dredentials", "true");
     res.header("Access-Control-Allow-Origin", "https://grapzmen-goanimation.github.io");
     next();
 });
